f3c62f40854cf69a36ec80f61aeba7c41906818b
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-10-26 14:14:59 +0300

    к
    к

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { useState } from 'react';
+
 export const App = () => {
+  const [value, setValue] = useState(0);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      {value}
+      <button type="button" onClick={() => setValue(value + 1)}>
+        Increment value by 1
+      </button>
     </div>
   );
 };
